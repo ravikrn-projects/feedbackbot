adff275a05cf0084e952c6031d7ed77ff4101952
Ravi Kumar Nimmi <rvkmr102@gmail.com>
2015-11-24 13:07:04 +0530

    sending response for pings

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
 import requests
 from config import token, base_url
 
+def send_response(user_id):
+	message = "What's up?"
+	url = base_url.format(token=token, method="sendMessage")
+	params = {'chat_id': user_id, 'text': message}
+	try:
+		response = requests.get(url, params=params).json()
+	except Exception as e:
+		print "Could not send message. error = {error}".format(error=e)
+	
 
-def callback():
+def callback(update_id):
 	url = base_url.format(token=token, method="getUpdates")
-	params = {}
+	params = {'offset': update_id}
 	try:
 		response = requests.get(url, params=params).json()
 	except Exception as e:
 		print "Could not get updates. error = {error}".format(error=e)
-	finally:
-		message_list = response["result"]
+	
+	message_list = response["result"]
+	if len(message_list) != 0:
 		for message in message_list:
 			print message['update_id'], message['message']['text']
-
-
+			send_response(message['message']['from']['id'])
+		update_id = message_list[-1]['update_id']
+	return update_id
 
 if __name__ == '__main__':
-	callback()
+	update_id = None
+	while True:
+		previous_update_id = callback(update_id)
+		if previous_update_id is not None:
+			update_id = previous_update_id + 1
---
+++ questions.py
@@ -0,0 +1,11 @@
+questions = [
+			{
+			"question": "Which television brand do you like?",
+			"choices": ["Samsung", "LG", "Micromax", "Other"]
+			},
+			{
+			"question": "Which laptop brand do you like?",
+			"choices": ["Apple", "Dell", "HP", "Other"]
+			}
+
+			]
