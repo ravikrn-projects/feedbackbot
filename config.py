ab29cb1a7a17f9d62c88b24cffe576b62a189a38
Ravi Kumar Nimmi <rvkmr102@gmail.com>
2015-11-24 18:28:15 +0530

    config_sample added

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 token = "172854231:AAECyx6B_r8xPt-sn29UEnRgiK2i1tKDh18" #freecoupon
 # token = "140944206:AAF3J7dYO2FaW7HJGQHRhh3RGgL-5HpUZMI" #supercool
 
+
 base_url = "https://api.telegram.org/bot{token}/{method}"
