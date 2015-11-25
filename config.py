a8a3b7bfc58a57b1834473347a7507ceb1ebc035
Ravi Kumar Nimmi <rvkmr102@gmail.com>
2015-11-25 12:33:14 +0530

    conflicts resolved

--- config.py
+++
@@ -1,5 +0,0 @@
-token = "172854231:AAECyx6B_r8xPt-sn29UEnRgiK2i1tKDh18" #freecoupon
-# token = "140944206:AAF3J7dYO2FaW7HJGQHRhh3RGgL-5HpUZMI" #supercool
-
-
-base_url = "https://api.telegram.org/bot{token}/{method}"
--- config_sample.py
+++ config_sample.py
@@ -1,7 +1,6 @@
-token = "172854231:AAECyx6B_r8xPt-sn29UEnRgiK2i1tKDh18" #freecoupon
-# token = "140944206:AAF3J7dYO2FaW7HJGQHRhh3RGgL-5HpUZMI" #supercool
-
+token = "140944206:AAF3J7dYO2FaW7HJGQHRhh3RGgL-5HpUZMI"
 base_url = "https://api.telegram.org/bot{token}/{method}"
+
 
 mongo_config = {
 		"host": "192.168.0.119",
